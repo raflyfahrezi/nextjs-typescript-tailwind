ad65d6a3a8d1e32c19ea8f8ef20e76086888dde8
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-24 17:10:10 +0700

    styling index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import type { NextPage } from 'next'
 
 const index: NextPage = () => {
-    return <div>Hello World</div>
+    return (
+        <div className='text-center my-20'>
+            <p>Hello World</p>
+        </div>
+    )
 }
 
 export default index
